668a1adcad21dc8a9d0f5f75001d8d1f57b23e16
Rafael José <ra.david@hotmail.com>
2023-06-03 22:34:53 +0100

    Add main menu and loading screen

---
+++ Assets/Scripts/ASyncLoader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ASyncLoader : MonoBehaviour
+{
+    [Header("Menu Screens")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject loadingScreen;
+
+    public void LoadLevel(int levelToLoad)
+    {
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+
+        StartCoroutine(LoadLevelASync(levelToLoad));
+    }
+
+    IEnumerator LoadLevelASync (int levelToLoad)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelToLoad);
+
+        yield return null;
+    }
+}
